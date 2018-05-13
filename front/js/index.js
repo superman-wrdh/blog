13ecf798577d1d6de4f1b7fa8e6ceb85d4c4281b
hc <13419655360@163.com>
2018-05-13 23:10:59 +0800

    前后端分离优化

--- front/js/index.js
+++ front/js/index.js
@@ -1,34 +1,108 @@
 $(document).ready(function () {
-    $.ajax({
-        url: 'http://www.66super.com/api/blog/list.do',
-        type: 'post',
-        dataType: 'json',
-        contentType : "application/json",
-        data:JSON.stringify({
+    p = 1
+    var host = "http://www.66super.com"
 
-        }),
+    function load_index_list(page) {
+        if (page === null) {
+            page = 1
+        }
+        //获取文章列表
+        $.ajax({
+            url: host + '/api/blog/list.do',
+            type: 'post',
+            dataType: 'json',
+            contentType: "application/json",
+            data: JSON.stringify({
+                "page": page
+            }),
+            async: false,
+            success: function (r) {
+                var res = r.data.results;
+                if (res.length < 10) {
+                    console.log('---没有数据了---')
+                    $("#load_div").text(" 没 有 数 据 了 ")
+                    setTimeout(function () {
+                        $("#next_page").remove();
+                    }, 2000);
+                }
+                for (var i = 0; i < res.length; i++) {
+                    var img = res[i]['imagesList']
+                    if (img.length == 0) {
+                        img = "<img src=\"images/t01.jpg\"/>"
+                    } else {
+                        img = img[0]
+                    }
+                    var li = "<li>\n" +
+                        "<h3 class=\"blogtitle\"><span><a href=\"/jstt/css3/\" title=\"css3\" target=\"_blank\"  class=\"classname\">个人博客</a></span><a href=\"info.html?bid=" + res[i]['id'] + "\" target=\"_blank\" >" + res[i]['title'] + "</a></h3>\n" +
+                        "<div class=\"bloginfo\"><span class=\"blogpic\"><a href=\"/jstt/css3/2018-03-26/812.html\" title=\"超锅的博客v5\">" + img + "</a></span>\n" +
+                        "<p>" + "<a href='info.html?bid=" + res[i]['id'] + "'" + " target='_blank'>" + res[i]['summary'] + "</a>" + "</p>\n" +
+                        "</div>\n" +
+                        "<div class=\"autor\"><span class=\"lm f_l\"></span><span class=\"dtime f_l\">" + res[i]['releaseDate'] + "</span><span class=\"viewnum f_l\">浏览数量（<a href=\"/\">+" + res[i]['clickHit'] + "</a>）</span><span class=\"f_r\"><a href=\"info.html?bid=" + res[i]['id'] + "\" class=\"more\">阅读原文>></a></span></div>\n" +
+                        "<div class=\"line\"></div>\n" +
+                        " </li>"
+                    $('#art_list').append(li);
+                }
+            }
+        });
+    }
+
+    load_index_list(p)
+
+    // 获取阅读量排行前5的文章
+    // http://www.66super.com/api/blog/listByReadTime/1.do
+    $.ajax({
+        url: host + '/api/blog/listByReadTime/1.do',
+        type: 'get',
+        dataType: 'json',
         async: false,
         success: function (r) {
             var res = r.data.results;
-            for(var i = 0;i<res.length;i++){
-                console.log(i)
+            res = res.slice(0, 5)
+            for (var i = 0; i < res.length; i++) {
+
                 var img = res[i]['imagesList']
-                if(img.length == 0){
+                if (img.length == 0) {
                     img = "<img src=\"images/t01.jpg\"/>"
-                }else {
+                } else {
                     img = img[0]
                 }
-                var li = "<li>\n" +
-                    "<h3 class=\"blogtitle\"><span><a href=\"/jstt/css3/\" title=\"css3\" target=\"_blank\"  class=\"classname\">个人博客</a></span><a href=\"info.html?bid="+res[i]['id']+"\" target=\"_blank\" >"+res[i]['title']+"</a></h3>\n" +
-                    "<div class=\"bloginfo\"><span class=\"blogpic\"><a href=\"/jstt/css3/2018-03-26/812.html\" title=\"帝国cms 首页或者列表无图，不使用默认图片的方法\">"+img+"</a></span>\n" +
-                    "<p>"+res[i]['summary']+"</p>\n" +
-                    "</div>\n" +
-                    "<div class=\"autor\"><span class=\"lm f_l\"></span><span class=\"dtime f_l\">"+res[i]['releaseDate']+"</span><span class=\"viewnum f_l\">浏览数量（<a href=\"/\">+"+res[i]['clickHit']+"</a>）</span><span class=\"f_r\"><a href=\"info.html?bid="+res[i]['id']+"\" class=\"more\">阅读原文>></a></span></div>\n" +
-                    "<div class=\"line\"></div>\n" +
+                var li = "<li><b><a href=\"info.html?bid=" + res[i]['id'] + "\" target=\"_blank\">" + res[i]['title'] + "</a></b>\n" +
+                    " <p><i><img src=\"images/t02.jpg\" /></i><a href=\"info.html?bid=" + res[i]['id'] + "\"" + " target=\"_blank\">" + res[i]['summary'].substring(0, 50) + "</a></p>\n" +
                     " </li>"
-                $('#art_list').append(li);
+                $('#hot_list').append(li);
             }
-
         }
     });
+
+    $("#next_page").click(function () {
+        p++
+        load_index_list(p)
+    })
+
+    $("#bt_search").click(function () {
+        mysearch()
+    })
+
+    function mysearch() {
+        var searchword=$("#searchword").val();
+        if(searchword.trim()=="" ||searchword.trim()==""){
+            alert("超锅提醒 关键字不能为空！请输入关键字搜索")
+            //***************
+            // $("#show_win").css("display","block")
+            //*******************
+            return false;
+        }else{
+            //ajax请求
+            //清空原来的列表
+            $("#art_list").children("li")[0].remove()
+
+            //ajax 后台请求 获取数据
+
+            //填充列表
+
+            //若果后台返回列表为空 填充一个结果()
+
+        }
+    }
+
 })
--- front/js/info.js
+++ front/js/info.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-    function GetQueryString(name)
-    {
-        var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    function GetQueryString(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
         var r = window.location.search.substr(1).match(reg);
-        if(r!=null)return  unescape(r[2]); return null;
+        if (r != null) return unescape(r[2]);
+        return null;
     }
 
     var bid = GetQueryString('bid')
     console.log(bid)
     $.ajax({
-        url: 'http://www.66super.com/api/blog/get/'+bid+'.do',
+        url: 'http://www.66super.com/api/blog/get/' + bid + '.do',
         type: 'get',
         dataType: 'json',
         async: false,
@@ -20,7 +20,63 @@ $(document).ready(function () {
             $('#b_date').append(r['releaseDate'])
             $('#b_click').append(r['clickHit'])
             var kwords = r['keyWord']
-            $('#tags').append("关键词  "+kwords)
+            $('#tags').append("关键词  " + kwords)
+        }
+    });
+
+    // /api/blog/next_and_previous/102.do
+    $.ajax({
+        url: 'http://www.66super.com/api/blog/next_and_previous/' + bid + '.do',
+        type: 'get',
+        dataType: 'json',
+        async: false,
+        success: function (r) {
+            var next = null
+            var last = null
+            if ("next" in r) {
+                next = r['next']
+            }
+            if("last" in r){
+                last = r['last']
+            }
+            if (last !== null) {
+                $("#last_blog").text(last['title'])
+                $("#last_blog").attr("href", "/info.html?bid=" + last['id']);
+            }
+
+            if (next !== null) {
+                $("#next_blog").text(next['title'])
+                $("#next_blog").attr("href", "/info.html?bid=" + last['id']);
+            }
+
+
+        }
+    });
+
+
+
+    function rnd(n, m) {
+        var random = Math.floor(Math.random() * (m - n + 1) + n);
+        return random;
+    }
+
+    $.ajax({
+        url: 'http://www.66super.com/api/blog/list.do',
+        type: 'post',
+        dataType: 'json',
+        contentType: "application/json",
+        data: JSON.stringify({
+            "page": rnd(1, 3)
+        }),
+        success: function (r) {
+            var res = r.data.results.slice(0, 6)
+            console.log(res)
+            //"<li><a href="/" title="云南之行――丽江古镇玉龙雪山">云南之行――丽江古镇玉龙雪山</a></li>"
+            for(var i = 0;i<res.length;i++){
+                var li = "<li><a href='info.html?bid="+res[i]['id']+"' title='云南之行――丽江古镇玉龙雪山'>"+res[i]['title']+"</a></li>"
+                $("#about_blog").append(li)
+            }
+
         }
     });
 
