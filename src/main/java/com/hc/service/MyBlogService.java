95a0e7e957cdda36377d0b8319d287a192c524d0
hc <13419655360@163.com>
2018-05-26 20:50:54 +0800

    get real ip when use proxy

--- src/main/java/com/hc/service/MyBlogService.java
+++ src/main/java/com/hc/service/MyBlogService.java
@@ -7,6 +7,7 @@ import com.hc.util.BlogUtil;
 import com.hc.util.DateUtil;
 import com.hc.util.RESUtil;
 import com.hc.util.StringUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -35,38 +36,38 @@ public class MyBlogService {
     @Autowired
     MusicMapper musicMapper;
 
-    private BlogIndex blogIndex=new BlogIndex();
+    private BlogIndex blogIndex = new BlogIndex();
 
-    public List<Blog> getByTime(){
-        return  blogMapper.countList();
+    public List<Blog> getByTime() {
+        return blogMapper.countList();
     }
 
     /**
      * 获取博客列表 带分页
      */
-    public PageBean list(SearchVo search){
-        Integer page=1;
-        if(search.getPage() != null){
-            page=search.getPage();
+    public PageBean list(SearchVo search) {
+        Integer page = 1;
+        if (search.getPage() != null) {
+            page = search.getPage();
         }
 
-        PageBean pageBean=new PageBean(page,10);
-        Map<String,Object> map= new HashMap<String,Object>();
+        PageBean pageBean = new PageBean(page, 10);
+        Map<String, Object> map = new HashMap<String, Object>();
         map.put("start", pageBean.getStart());
         map.put("size", pageBean.getPageSize());
-        if(search.getBlogType() != null){
-             map.put("typeId",search.getBlogType());
+        if (search.getBlogType() != null) {
+            map.put("typeId", search.getBlogType());
         }
-        if(search.getReleaseDateStr() != null && StringUtil.isNotEmpty(search.getReleaseDateStr())){
-             map.put("releaseDateStr",search.getReleaseDateStr());
+        if (search.getReleaseDateStr() != null && StringUtil.isNotEmpty(search.getReleaseDateStr())) {
+            map.put("releaseDateStr", search.getReleaseDateStr());
         }
         List<Blog> list = blogMapper.list(map);
-        for(Blog blog:list){
-            BlogUtil.fillBlogImg(blog,"jpg");
+        for (Blog blog : list) {
+            BlogUtil.fillBlogImg(blog, "jpg");
         }
 
-        for(Blog blog:list){
-            BlogUtil.fillBlogImg(blog,"png");
+        for (Blog blog : list) {
+            BlogUtil.fillBlogImg(blog, "png");
         }
 
         pageBean.setResultSize(list.size());
@@ -77,26 +78,26 @@ public class MyBlogService {
     /**
      * lucene搜索分词
      */
-    public PageBean search(String word,Integer page)throws Exception{
-        if(page<=0 || page == null){
-            page=1;
+    public PageBean search(String word, Integer page) throws Exception {
+        if (page <= 0 || page == null) {
+            page = 1;
         }
-        List<Blog> blogList=blogIndex.searchBlog(word.trim());
-        PageBean pageBean = new PageBean(page,10);
+        List<Blog> blogList = blogIndex.searchBlog(word.trim());
+        PageBean pageBean = new PageBean(page, 10);
         pageBean.setResultSize(blogList.size());
         pageBean.setResults(blogList);
         return pageBean;
     }
 
     /**
-     *按照阅读量查询博客
+     * 按照阅读量查询博客
      */
-    public PageBean listByReadTime(Integer page){
-        if(page<=0){
-            page=1;
+    public PageBean listByReadTime(Integer page) {
+        if (page <= 0) {
+            page = 1;
         }
-        PageBean pageBean=new PageBean(page,10);
-        Map<String,Object> map= new HashMap<String,Object>();
+        PageBean pageBean = new PageBean(page, 10);
+        Map<String, Object> map = new HashMap<String, Object>();
         map.put("start", pageBean.getStart());
         map.put("size", pageBean.getPageSize());
         List<Blog> list = blogMapper.getBlogByClickHitTime(map);
@@ -107,96 +108,98 @@ public class MyBlogService {
 
     /**
      * 获取上一篇 下一篇
+     *
      * @param id
      * @return
      */
-    public Map<String,Blog> getLastAndPrivious(int id){
+    public Map<String, Blog> getLastAndPrivious(int id) {
         Blog last = blogMapper.getLastBlog(id);
         Blog next = blogMapper.getNextBlog(id);
-        Map<String,Blog> r = new HashMap<>();
-        r.put("last",last);
-        r.put("next",next);
+        Map<String, Blog> r = new HashMap<>();
+        r.put("last", last);
+        r.put("next", next);
         return r;
     }
 
     /**
      * 获取轮播图
      */
-    public List<String> getBanner(){
+    public List<String> getBanner() {
         String imgs = RESUtil.getString("img");
-        String[] imgArr=imgs.split("\\+");
+        String[] imgArr = imgs.split("\\+");
         List<String> list = Arrays.asList(imgArr);
         return list;
     }
 
     /**
-     *统计每个模块的阅读人数 算法实现
+     * 统计每个模块的阅读人数 算法实现
      */
-    public Map<String,Integer> getClickCount(){
+    public Map<String, Integer> getClickCount() {
         List<Blog> blogs = blogMapper.selectByCountClick();
-        List<String> result=new ArrayList<String>();
-        for (int i = 0; i <blogs.size() ; i++) {
-            result.add(blogs.get(i).getBlogType().getTypeName()+"#"+blogs.get(i).getClickHit());
+        List<String> result = new ArrayList<String>();
+        for (int i = 0; i < blogs.size(); i++) {
+            result.add(blogs.get(i).getBlogType().getTypeName() + "#" + blogs.get(i).getClickHit());
         }
-        for (int i=0;i<result.size()-1;i++){
-            for(int j=i+1;j<result.size();j++){
-                if(result.get(i).split("\\#")[0].equals(result.get(j).split("\\#")[0])){
-                    result.set(i,result.get(i).split("\\#")[0]+"#"+(Integer.parseInt(result.get(i).split("\\#")[1])+Integer.parseInt(result.get(j).split("\\#")[1])));
+        for (int i = 0; i < result.size() - 1; i++) {
+            for (int j = i + 1; j < result.size(); j++) {
+                if (result.get(i).split("\\#")[0].equals(result.get(j).split("\\#")[0])) {
+                    result.set(i, result.get(i).split("\\#")[0] + "#" + (Integer.parseInt(result.get(i).split("\\#")[1]) + Integer.parseInt(result.get(j).split("\\#")[1])));
                     result.remove(j);
-                    j=i;
+                    j = i;
                 }
             }
         }
-        Map<String,Integer> map = new HashMap<String, Integer>();
-        for (int i = 0; i <result.size() ; i++) {
-            map.put(result.get(i).split("\\#")[0],Integer.parseInt(result.get(i).split("\\#")[1]));
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (int i = 0; i < result.size(); i++) {
+            map.put(result.get(i).split("\\#")[0], Integer.parseInt(result.get(i).split("\\#")[1]));
         }
         return map;
     }
 
     /**
      * 统计每个模块的阅读人数 废弃
+     *
      * @return
      */
-    public Map<String,Integer> getClickCountFromDB(){
+    public Map<String, Integer> getClickCountFromDB() {
         List<Blog> blogs = blogMapper.countSearchWord();
 
-        Map<String,Integer> map = new HashMap<String, Integer>();
+        Map<String, Integer> map = new HashMap<String, Integer>();
         for (Blog blog : blogs) {
-            map.put(blog.getKeyWord(),blog.getClickHit());
+            map.put(blog.getKeyWord(), blog.getClickHit());
         }
         return map;
     }
 
-    public Map<String,Integer> getModuleClick(){
+    public Map<String, Integer> getModuleClick() {
         List<Blog> blogs = blogMapper.countModuleClick();
-        Map<String,Integer> map = new HashMap<String, Integer>();
+        Map<String, Integer> map = new HashMap<String, Integer>();
         for (Blog blog : blogs) {
-            map.put(blog.getTitle(),blog.getClickHit());
+            map.put(blog.getTitle(), blog.getClickHit());
         }
         return map;
     }
 
-    public List<Blog> getEachBlogClick(){
+    public List<Blog> getEachBlogClick() {
         return blogMapper.selectByCountClick();
     }
 
     /**
      * 关键词
      */
-    public List<SearchWord> searchWordlist(){
+    public List<SearchWord> searchWordlist() {
         return searchWordMapper.list();
     }
 
-    public void insertOrUpdate(SearchWord searchWord){
+    public void insertOrUpdate(SearchWord searchWord) {
         SearchWord old = searchWordMapper.findByWord(searchWord.getWord());
-        if(old!=null){
-            if(searchWord.getWord().equals(old.getWord())){
-                searchWord.setCount((old.getCount()+1));
+        if (old != null) {
+            if (searchWord.getWord().equals(old.getWord())) {
+                searchWord.setCount((old.getCount() + 1));
                 searchWord.setId(old.getId());
                 searchWordMapper.update(searchWord);
             }
-        }else{
+        } else {
             searchWord.setCount(1);
             searchWordMapper.insert(searchWord);
         }
@@ -206,22 +209,22 @@ public class MyBlogService {
     /**
      * 每天第一次访问计数器为1，当天中随后每次访问加1
      */
-    public void VistorCount(){
+    public void VistorCount() {
         Date now = new Date();
         String nowDateStr = DateUtil.formatDate(now, "yyyy-MM-dd");
         List<VisitorRecord> list = visitorRecordMapper.list(null);
-        Integer idIndex=null;
+        Integer idIndex = null;
         for (VisitorRecord record : list) {
-            if(DateUtil.formatDate(record.getDate(),"yyyy-MM-dd").equals(nowDateStr)){
-                idIndex=record.getId();
+            if (DateUtil.formatDate(record.getDate(), "yyyy-MM-dd").equals(nowDateStr)) {
+                idIndex = record.getId();
                 System.out.println(record.getId());
-                VisitorRecord forUpdate= visitorRecordMapper.getById(record.getId());
-                forUpdate.setCount(record.getCount()+1);
+                VisitorRecord forUpdate = visitorRecordMapper.getById(record.getId());
+                forUpdate.setCount(record.getCount() + 1);
                 visitorRecordMapper.update(forUpdate);
             }
         }
-        if(idIndex==null){
-            VisitorRecord forInsert=new VisitorRecord();
+        if (idIndex == null) {
+            VisitorRecord forInsert = new VisitorRecord();
             forInsert.setCount(1);
             forInsert.setDate(new Date());
             visitorRecordMapper.insert(forInsert);
@@ -230,18 +233,19 @@ public class MyBlogService {
 
     /**
      * 获取最近游客访问次数
+     *
      * @param day
      * @return
      */
-    public List<VisitorRecord> getRecord(int day){
-        Map<String,Object> map = new HashMap<>();
-        int max=visitorRecordMapper.count();
-        int lastOfRecord=day;
-        if(day>=max){
-            lastOfRecord=max;
+    public List<VisitorRecord> getRecord(int day) {
+        Map<String, Object> map = new HashMap<>();
+        int max = visitorRecordMapper.count();
+        int lastOfRecord = day;
+        if (day >= max) {
+            lastOfRecord = max;
         }
-        map.put("start",max-lastOfRecord);
-        map.put("size",lastOfRecord);
+        map.put("start", max - lastOfRecord);
+        map.put("size", lastOfRecord);
         List<VisitorRecord> list = visitorRecordMapper.list(map);
         return list;
     }
@@ -249,28 +253,40 @@ public class MyBlogService {
     /**
      * 获取登录记录
      */
-    public List<LoginRecord> getLoginRecords(int time){
-        Map<String,Object> map = new HashMap<>();
-        int max=loginRecordMapper.count();
-        int lastOfRecord=time;
-        if(time>=max){
-            lastOfRecord=max;
+    public List<LoginRecord> getLoginRecords(int time) {
+        Map<String, Object> map = new HashMap<>();
+        int max = loginRecordMapper.count();
+        int lastOfRecord = time;
+        if (time >= max) {
+            lastOfRecord = max;
         }
-        map.put("start",max-lastOfRecord);
-        map.put("size",lastOfRecord);
+        map.put("start", max - lastOfRecord);
+        map.put("size", lastOfRecord);
         List<LoginRecord> list = loginRecordMapper.list(map);
         return list;
     }
 
+    public String getUserIp(HttpServletRequest request) {
+        // try get real ip if user use proxy
+        String ip = request.getHeader("x-forwarded-for");
+        if (StringUtils.isBlank(ip))
+            return request.getRemoteAddr();
+        else if (ip.indexOf(',') > 0)
+            return ip.split(",")[0];
+        else
+            return ip;
+    }
+
     /**
      * 产生一条登录记录
+     *
      * @param request
      * @param status
      * @param loginName
      */
-    public void createOneLoginRecord(HttpServletRequest request,String status,String loginName){
-        String ip = request.getRemoteAddr();
-        LoginRecord loginRecord=new LoginRecord();
+    public void createOneLoginRecord(HttpServletRequest request, String status, String loginName) {
+        String ip = getUserIp(request);
+        LoginRecord loginRecord = new LoginRecord();
         loginRecord.setStatus(status);
         loginRecord.setIp(ip);
         loginRecord.setLoginName(loginName);
@@ -279,7 +295,7 @@ public class MyBlogService {
     }
 
 
-    public List<Music> MusicList(){
+    public List<Music> MusicList() {
         return musicMapper.list();
     }
 
